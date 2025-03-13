974097acf437693131ba4a9c7980c3e25875f1a1
nacho613 <duddbs0613@gmail.com>
2025-03-13 22:30:26 +0900

    3131030

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    reactStrictMode: true,
+    experimental: {
+      appDir: true, // App Router 사용
+    },
+  };
+  
 export default nextConfig;
