d2cc73c633c9b9e3788180cf60fdedbe9466080e
nacho613 <duddbs0613@gmail.com>
2025-03-24 22:43:53 +0900

    fix : type error 수정 category/[name]/page 코드 최소화. 당장 사용안하고 나중에 사용할 코드

--- apps/web/app/category/[name]/page.tsx
+++ apps/web/app/category/[name]/page.tsx
@@ -1,24 +1,9 @@
-import { categories } from '../../lib/categoryData';
-import Link from 'next/link';
-import { notFound } from 'next/navigation';
-
-// Next.JS 15부터 params(url 값)이 비동기(promise)일 수 있다.
-// 그렇기에 async + await로 사용해야 안정적.
-
 export default async function Page({
   params,
 }: {
-  params: { name: string } | Promise<{ name: string }>;
+  params: { name: string };
 }) {
-  const awaitedParams = await params;
-  const { name } = awaitedParams;
-
-  const current = categories.find((c) => c.name === name);
-  // undifinde 등 예외값 처리, 이거 없으면 에러 뜸
-  // if (!current) return notFound();
-
-  const children = categories.filter((c) => c.parent === name);
-  // const parent = current.parent;
+  const { name } = params;
 
   return (
     <main>
